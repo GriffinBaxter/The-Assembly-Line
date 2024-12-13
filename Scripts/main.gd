6e386bdf00ed24a4148e0d8d0c5d5a3780be4bd5
Griffin Baxter <GriffinRBaxter@gmail.com>
2024-12-14 11:24:00 +1300

    Add cake assembly with candle

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -10,11 +10,13 @@ func _ready() -> void:
 	while true:
 		var assembly_index = rng.randi_range(0, assemblies.size() - 1)
 		assembly = assemblies[assembly_index].instantiate()
-		assembly.position = Vector3(-10, 2.375, -2.5)
 		get_tree().get_root().add_child.call_deferred(assembly)
 		await assembly.ready
 
-		get_tree().create_tween().tween_property(assembly, "position", Vector3(10, 2.375, -2.5), 15)
+		assembly.position += Vector3(-10, 1.9, -2.5)
+		get_tree().create_tween().tween_property(
+			assembly, "position", Vector3(10, assembly.position.y, -2.5), 15
+		)
 		await get_tree().create_timer(15).timeout
 
 		get_tree().get_root().remove_child(assembly)
